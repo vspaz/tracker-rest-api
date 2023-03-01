802df9d99a04ddd7d2cde204d2826e34fc1ea2e8
vspaz <vladimir.edelman@gmail.com>
2023-03-01 18:57:53 +0100

    add HealthStatus handler

--- handlers/health.go
+++ handlers/health.go
@@ -1,1 +1,11 @@
 package handlers
+
+import (
+	"github.com/go-chi/render"
+	"net/http"
+)
+
+func (r *Router) GetHealthStatus(response http.ResponseWriter, request *http.Request) {
+	render.Status(request, http.StatusOK)
+	render.JSON(response, request, map[string]string{"ping": "pong"})
+}
