8a17a9d504206afa05ab61335d143e54fa87d28a
vspaz <vladimir.edelman@gmail.com>
2023-03-01 19:10:43 +0100

    add config

--- config/config.go
+++ config/config.go
@@ -1,1 +1,56 @@
 package config
+
+import (
+	"sync"
+	"time"
+)
+
+var (
+	config *SingletonConfig
+	once   sync.Once
+)
+
+type SingletonConfig struct {
+	Config *Conf
+}
+
+type Conf struct {
+	Http *Http
+}
+
+type Http struct {
+	*Server
+}
+
+type Server struct {
+	HostAndPort             string
+	ReadTimeout             time.Duration
+	WriteTimeout            time.Duration
+	IdleTimeout             time.Duration
+	RequestExecutionTimeout time.Duration
+}
+
+func initConfig() *SingletonConfig {
+	return &SingletonConfig{
+		Config: &Conf{
+			Http: &Http{
+				Server: &Server{
+					HostAndPort:             ":8080",
+					ReadTimeout:             10 * time.Second,
+					WriteTimeout:            10 * time.Second,
+					IdleTimeout:             10 * time.Second,
+					RequestExecutionTimeout: 10 * time.Second,
+				},
+			},
+		},
+	}
+}
+
+func GetConfig() *SingletonConfig {
+	once.Do(
+		func() {
+			config = initConfig()
+		},
+	)
+	return config
+}
