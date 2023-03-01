02ea5a69ba575eae3506361f6e87375b7e73b334
vspaz <vladimir.edelman@gmail.com>
2023-03-01 19:14:34 +0100

    add constructor NewRounter

--- handlers/router.go
+++ handlers/router.go
@@ -3,9 +3,19 @@ package handlers
 import (
 	"github.com/go-chi/chi/v5"
 	"github.com/sirupsen/logrus"
+	"github.com/vspaz/tracker-rest-api/config"
 )
 
 type Router struct {
 	Logger *logrus.Logger
+	Conf   *config.Conf
 	mux    *chi.Mux
+}
+
+func NewRouter(conf *config.Conf, logger *logrus.Logger) *Router {
+	return &Router{
+		Logger: logger,
+		Conf:   conf,
+		mux:    chi.NewRouter(),
+	}
 }
