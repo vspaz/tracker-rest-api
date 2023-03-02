fedc08e5077a996be3977c02abd9449acc9ef763
vspaz <vladimir.edelman@gmail.com>
2023-03-02 19:30:17 +0100

    add Index.go

--- handlers/index.go
+++ handlers/index.go
@@ -1,1 +1,16 @@
 package handlers
+
+import (
+	"net/http"
+	"strconv"
+)
+
+func (r *Router) Index(response http.ResponseWriter, request *http.Request) {
+	okBody := []byte("Ok")
+	response.WriteHeader(http.StatusOK)
+	response.Header().Set("Content-Length", strconv.Itoa(len(okBody)))
+	_, err := response.Write(okBody)
+	if err != nil {
+		r.Logger.Errorf("error occurred: %s", err.Error())
+	}
+}
