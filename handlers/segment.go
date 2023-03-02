95250a923cfceca6b87fa006e20da376e6f32349
vspaz <vladimir.edelman@gmail.com>
2023-03-02 21:16:51 +0100

    add Page stub

--- handlers/segment.go
+++ handlers/segment.go
@@ -24,3 +24,8 @@ func (r *Router) Alias(response http.ResponseWriter, request *http.Request) {
 	render.Status(request, http.StatusOK)
 	render.JSON(response, request, map[string]string{"status": "200 OK", "message": "OK"})
 }
+
+func (r *Router) Page(response http.ResponseWriter, request *http.Request) {
+	render.Status(request, http.StatusOK)
+	render.JSON(response, request, map[string]string{"status": "200 OK", "message": "OK"})
+}
