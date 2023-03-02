fb66ebf212e3dfc2062a1caa6456fc0fa85ae5ef
vspaz <vladimir.edelman@gmail.com>
2023-03-02 21:14:46 +0100

    add Identify endpoint

--- handlers/segment.go
+++ handlers/segment.go
@@ -9,3 +9,8 @@ func (r *Router) Track(response http.ResponseWriter, request *http.Request) {
 	render.Status(request, http.StatusOK)
 	render.JSON(response, request, map[string]string{"status": "200 OK", "message": "OK"})
 }
+
+func (r *Router) Identify(response http.ResponseWriter, request *http.Request) {
+	render.Status(request, http.StatusOK)
+	render.JSON(response, request, map[string]string{"status": "200 OK", "message": "OK"})
+}
