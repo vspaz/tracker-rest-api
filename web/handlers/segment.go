f2444c9766fb09c66b9bb9e71e6af3acfa5b227d
vspaz <vladimir.edelman@gmail.com>
2023-03-11 14:25:37 +0100

    add handleSingleRequest

--- web/handlers/segment.go
+++ web/handlers/segment.go
@@ -1,9 +1,25 @@
 package handlers
 
 import (
+	"encoding/json"
+	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/render"
 	"net/http"
 )
+
+func (r *Router) handleSingleRequest(response http.ResponseWriter, request *http.Request) {
+	writeKey := chi.URLParam(request, "writeKey")
+	var eventBody Event
+	if err := json.NewDecoder(request.Body).Decode(&eventBody); err != nil {
+		r.Logger.Errorf("failed to parse payload %s", err)
+		render.Status(request, http.StatusBadRequest)
+		render.JSON(response, request, map[string]string{"status": "400", "message": "Bad Request"})
+		return
+	}
+	println(writeKey, eventBody)
+	render.Status(request, http.StatusOK)
+	render.JSON(response, request, map[string]string{"status": "200 OK", "message": "OK"})
+}
 
 func (r *Router) Track(response http.ResponseWriter, request *http.Request) {
 	render.Status(request, http.StatusOK)
