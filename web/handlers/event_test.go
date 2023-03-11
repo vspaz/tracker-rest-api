2c9d869fc68a2ed7665366e0960e9da9541e949a
vspaz <vladimir.edelman@gmail.com>
2023-03-11 21:40:08 +0100

    extend the test

--- web/handlers/event_test.go
+++ web/handlers/event_test.go
@@ -6,10 +6,14 @@ import (
 	"testing"
 )
 
-func TestAgentViewServer(t *testing.T) {
-	var body = []byte(`{"userId": "some user", "writeKey": "some key"}`)
+func TestEventDeserializationOk(t *testing.T) {
+	var body = []byte(`{"sentAt":"2023-03-11T19:23:48.022Z","timestamp":"2023-03-11T19:23:42.877Z","event":"something_happened","type":"track","userId":"12345"}`)
 	var eventBody Event
 	err := json.Unmarshal(body, &eventBody)
 	assert.Nil(t, err)
-	assert.Equal(t, "some user", eventBody.UserId)
+	assert.Equal(t, "2023-03-11T19:23:48.022Z", eventBody.SentAt)
+	assert.Equal(t, "2023-03-11T19:23:42.877Z", eventBody.Timestamp)
+	assert.Equal(t, "something_happened", eventBody.Event)
+	assert.Equal(t, "track", eventBody.EventType)
+	assert.Equal(t, "12345", eventBody.UserId)
 }
