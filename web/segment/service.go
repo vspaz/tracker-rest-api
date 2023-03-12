ca35ed115e7252adf094c8d8d0095885188c12f3
vspaz <vladimir.edelman@gmail.com>
2023-03-12 20:16:16 +0100

    add DefaultSegmentService

--- web/segment/service.go
+++ web/segment/service.go
@@ -1,1 +1,19 @@
 package segment
+
+import (
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/sirupsen/logrus"
+)
+
+const (
+	ERROR_INVALID_WRITE_KEY = "invalid write key"
+	ERROR_EMPTY_BATCH       = "Batch is empty"
+	ERROR_MESSAGE_TOO_BIG   = "Event is too big"
+	ERROR_UNEXPECTED        = "Unexpected error"
+)
+
+type DefaultSegmentService struct {
+	Logger          *logrus.Logger
+	WriteKeyToTopic map[string]string
+	KafkaProducer   *kafka.Producer
+}
